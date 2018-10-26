498d84b28b1f63b54970a3b7bf937df068804f41
Amin Taghikhani <amin76taghikhani@gmail.com>
2018-10-26 10:55:57 +0330

    Add Message

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Token = require('./lib/token')
 const VerificationCode = require('./lib/verificationCode')
+const Message = require('./lib/message')
 module.exports = {
     Token,
-    VerificationCode
+    VerificationCode,
+    Message
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
 const Token = require('../lib/token')
 const VerificationCode = require('../lib/verificationCode')
+const Message = require('../lib/message')
 const token = new Token();
-const verificationCode = new VerificationCode();
-token.get('APIKEY','SECRETKEY').then((token)=>{
-    verificationCode.send(token,'09xxxxxxxxx',12345).then(console.log).catch(console.log)
+const msg = new Message();
+token.get('42c2abcc65a6b16b33ae9f07', 'AFyNhpg5NQAC8Z8SDwSi').then(async (token)=>{
+    const result = await msg.send(token,['09359395520'],["سلام"],"30004747476131")
+    console.log(result);
+    
 });
