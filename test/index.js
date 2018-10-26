4857f2b63e9765de5312a2012eaa4ee8a2b8f7c6
Amin Taghikhani <amin76taghikhani@gmail.com>
2018-10-26 11:02:39 +0330

    Reformat Code

--- test/index.js
+++ test/index.js
@@ -2,9 +2,24 @@ const Token = require('../lib/token')
 const VerificationCode = require('../lib/verificationCode')
 const Message = require('../lib/message')
 const token = new Token();
+const verificationCode = new VerificationCode();
 const msg = new Message();
-token.get('42c2abcc65a6b16b33ae9f07', 'AFyNhpg5NQAC8Z8SDwSi').then(async (token)=>{
-    const result = await msg.send(token,['09359395520'],["سلام"],"30004747476131")
-    console.log(result);
-    
-});
+const APIKEY = '';
+const SECRET = '';
+const PHONENUMBER = '';
+const LINENUMBER = ''
+
+async function tokenTest() {
+    const result = await token.get(APIKEY, SECRET)
+    console.log(result)
+}
+async function verificationTest() {
+    const tkn = await token.get(APIKEY, SECRET)
+    const result = await verificationCode.send(tkn, PHONENUMBER, 12345)
+    console.log(result)
+}
+async function messageTest() {
+    const tkn = await token.get(APIKEY, SECRET)
+    const result = await msg.send(tkn, [PHONENUMBER], ['message'], LINENUMBER)
+    console.log(result)
+}
